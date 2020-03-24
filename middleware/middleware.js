4e56a27716583bbde2e46a073d2171701d3ee534
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-03-24 17:26:07 +0300

    fixed the work of the route of processing a nonexistent address

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('*', (req, res, next) => {
+router.all('*', (req, res, next) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
   next();
 });
