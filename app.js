1bbe95b47edffabefff6b659830f19c008e40872
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-04-17 15:44:59 +0300

    corrected error handling code

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(errorLogger);
 app.use(middleware);
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.srarus(statusCode)
+  res.status(statusCode)
     .send({
       message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
     });
